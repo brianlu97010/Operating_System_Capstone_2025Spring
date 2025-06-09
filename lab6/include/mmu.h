747dd257621a9cb8a049cef8ca0fe04de1ceb0db
Brian(Ubuntu) <brianlu97010@gmail.com>
2025-06-10 07:11:03 +0800

    Features: implement kernel space three level translation

--- lab6/include/mmu.h
+++ lab6/include/mmu.h
@@ -1,6 +1,21 @@
 #ifndef _MMU_H
 #define _MMU_H
 
+/* Translate the kernel virtual address to physical address */
+#define KERNEL_VA_BASE    0xFFFF000000000000
+#define VIRT_TO_PHYS(va)  ((unsigned long)(va) - KERNEL_VA_BASE)
+#define PHYS_TO_VIRT(pa)  ((unsigned long)(pa) + KERNEL_VA_BASE)
+
+// First 1GB: RAM and GPU peripherals
+// Physical addresses range from 0x3C000000 to 0x3CFFFFFF for peripherals (Ref : BCM2837 ARM Peripherals Manual)
+#define RAM_START                       0x00000000
+#define RAM_END                         0x3C000000
+#define PERIPHERAL_START                0x3C000000    // It will be access in user program
+#define PERIPHERAL_END                  0x40000000
+
+#define BLOCK_SIZE_2MB                  0x200000
+
+
 /* 
  *********** Translation Control Register (TCR) Configuration ***********
  * T1SZ, `bits [21:16]` = `10000`
@@ -53,10 +68,22 @@
  *  `Bits[47:12]` : the address of the required next-level table
  *  `Bits[11:0]` of the table address are zero.
  */
+#define PD_VALID                         0b1
 #define PD_TABLE                         0b11
 #define PD_BLOCK                         0b01
 #define PD_ACCESS                       (1 << 10)
+#define PD_USER                         (1 << 6)   // 0 for only kernel access, 1 for user/kernel access.
+#define PD_READONLY                     (1 << 7)   // 0 for read-write, 1 for read-only (Note that If you set Bits[7:6] to 0b01, which means the user can read/write the region, then the kernel is automatically not executable in that region no matter what the value of Bits[53] is.)
+
+// Page Table Entry Attribute for kernel space 
 #define BOOT_PGD_ATTR                   PD_TABLE
-#define BOOT_PUD_ATTR                   (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
+// #define BOOT_PUD_ATTR                   (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK) // this is block descriptor, for two level translation
+#define BOOT_PUD_ATTR                   PD_TABLE
+#define BOOT_PMD_ATTR_RAM              (PD_ACCESS | (MAIR_IDX_NORMAL_NOCACHE << 2) | PD_BLOCK) // this is block descriptor, for 0x00000000 ~ 0x3F000000 set to normal non-cache memory
+#define BOOT_PMD_ATTR_PERIPHERAL       (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK) // this is block descriptor, for 0x3F000000 ~ 0x40000000 and 0x40000000 ~ 0x7FFFFFFFFF set to device nGnRnE memory 
+
+// Page Table Entry Attribute for user spce
+#define USER_TABLE_ATTR                 PD_TABLE
+#define USER_PTE_ATTR                  (PD_ACCESS | PD_USER | (MAIR_IDX_NORMAL_NOCACHE << 2) | PD_TABLE)
 
 #endif
